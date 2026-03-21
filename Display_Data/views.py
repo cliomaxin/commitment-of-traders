f089cd7f5ff8080416c84cca4e04d7fdaf983427
maxipytech <maxipytech@gmail.com>
2026-03-21 18:57:39 +0000

    data imported and displayed successfully and index html modified to display data

--- Display_Data/views.py
+++ Display_Data/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from Handle_Raw_COT.models import CotReport
 
 # Create your views here.
 def index(request):
-    return render(request, 'Display/index.html')
+    reports = CotReport.objects.order_by('-as_of_date', 'name').all()
+    latest_date = reports.first().as_of_date if reports else None
+    return render(request, 'Display/index.html', {
+        'reports': reports,
+        'instruments_count': reports.count(),
+        'latest_date': latest_date,
+    })
